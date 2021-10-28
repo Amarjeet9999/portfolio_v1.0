0c255b0d5b01a02a5f1ec5aea1cb386c7853aaff
Amarjeet Kumar <amarjeetyt@gmail.com>
2021-10-28 19:13:24 +0530

    Contact Form Working

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,28 +1,67 @@
 /** @format */
 
 import React from "react";
+import emailjs from "emailjs-com";
 import "./Contact.css";
 
 export const Contact = () => {
+  const sendEmail = (e) => {
+    console.log(e.traget);
+
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_fqmd8g6",
+        "template_9aq7ife",
+        e.target,
+        "user_m8LKXMpcOx6HR8IyeFhv8"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <>
-      <section className='contact' id='contact'>
-        <h1 className='heading'>
+      <section className="contact" id="contact">
+        <h1 className="heading">
           <span>Contact</span> me{" "}
         </h1>
 
-        <form action='' onSubmit={(e) => e.preventDefault()}>
-          <input type='text' placeholder='Your Name' className='box' />
-          <input type='email' placeholder='Your Email' className='box' />
-          <input type='text' placeholder='Subject' className='box' />
+        <form action="" onSubmit={sendEmail}>
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="box"
+            name="name"
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            className="box"
+            name="email"
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            className="box"
+            name="subject"
+          />
           <textarea
-            name=''
-            className='box'
-            placeholder='your message'
-            id=''
-            cols='30'
-            rows='10'></textarea>
-          <input type='submit' value='Send Message' className='btn' />
+            name="message"
+            className="box"
+            placeholder="your message"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <input type="submit" value="Send Message" className="btn" />
         </form>
       </section>
     </>
