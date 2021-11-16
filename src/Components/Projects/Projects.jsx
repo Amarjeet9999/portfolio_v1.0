aa5b84d9082e6235f695981f1ff10d66746a620a
Amaarjeet Kumar <amarjeetyt@gmail.com>
2021-11-16 21:36:01 +0530

    Improved Little CSS

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -16,6 +16,48 @@ export const Projects = () => {
 
         <div className="box-container">
           <div className="box">
+            <img src="/Images/reddit.png" alt="" />
+            <div className="content">
+              <h3>Reddit Clone</h3>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  width: "40%",
+                }}
+              >
+                <a
+                  className="icons-git"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://github.com/Amarjeet9999/full-stack-reddit"
+                >
+                  <img
+                    style={{ height: "100%", width: "100%" }}
+                    src="/Images/github-brands.svg"
+                    alt=""
+                  />
+                </a>
+                <a
+                  className="icons-git"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://full-stack-reddit-clone.netlify.app"
+                >
+                  <img
+                    style={{ height: "100%", width: "100%" }}
+                    src="/Images/globe-solid.svg"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <NewDiv>
+              <h3>Built with React, Redux, MongoDB and JavaScript</h3>
+            </NewDiv>
+          </div>
+          <div className="box">
             <img src="/Images/explorepage.png" alt="" />
             <div className="content">
               <h3>Unacademy Clone</h3>
@@ -54,7 +96,7 @@ export const Projects = () => {
               </div>
             </div>
             <NewDiv>
-              <h3>Built with React, JSX and JavaScipt.</h3>
+              <h3>Built with React, JSX and JavaScript.</h3>
             </NewDiv>
           </div>
 
